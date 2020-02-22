ec21412806eed67ce1061701b98ae2bb8cd59799
wasilahmad <wasilinfo@gmail.com>
2020-02-23 00:46:03 +0530

    added cart-icon into header and cart dropdown toggle effect

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -2,33 +2,42 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 
 
-const Header = ({currentUser}) => {
+const Header = ({currentUser, hidden}) => {
     return (
         <div className='header'>
             <Link className='logo-container' to='/'>
                 <Logo className='logo' />
             </Link>
             <div className='options'>
-            <Link className='option' to='/shop'>
-                SHOP
-            </Link>
-            {
-                currentUser ? (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>) : (<Link className='option' to='/sign-in'>SIGN IN</Link> )
-            }
-            <Link className='option' to='/'>
-                CONTACT
-            </Link>
+                <Link className='option' to='/shop'>
+                    SHOP
+                </Link>
+                {
+                    currentUser ? (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>) : (<Link className='option' to='/sign-in'>SIGN IN</Link> )
+                }
+                <Link className='option' to='/'>
+                    CONTACT
+                </Link>
+                <CartIcon />
             </div>
+            {
+                hidden ? null : <CartDropdown />
+            }               
         </div>
     );
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
