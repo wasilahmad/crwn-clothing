5c22ed6ffb9b671f4b1e300da01f4425c96d4518
wasilahmad <wasilinfo@gmail.com>
2020-03-03 01:37:25 +0530

    adding and mapping firestore shop data to redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -70,7 +70,11 @@ export const convertCollectionsSnapshotToMap = (collections) => {
         }
     });
 
-    console.log('transformedCollection:', transformedCollection);
+    // console.log('transformedCollection:', transformedCollection);
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
 }
 
 export const auth = firebase.auth();
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './shop.styles.scss';
 
 import CollectionOverview from '../../component/collection-overview/collection-overview.component';
 import CollectionPage from '../collection/collection.component';
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
 
 class Shop extends React.Component {
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
+        const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
-        collectionRef.onSnapshot( async snapshot => {
-            console.log(snapshot);
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot( async snapshot => {
+            // console.log(snapshot);
+            
             // to check converted firebase shop collections data
-            convertCollectionsSnapshotToMap(snapshot);
-        })
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            // console.log(collectionsMap);
+
+            // call dispath props here to update collections
+             updateCollections(collectionsMap);
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromSnapshot();
     }
 
     render(){
@@ -28,6 +40,10 @@ class Shop extends React.Component {
             </div>
         );
     }
-}  
+}
 
-export default Shop;
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+
+export default connect(null, mapDispatchToProps)(Shop);
